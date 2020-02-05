78d98e2a8738e6ebc9e4e1e7f34c02a7c24fe0e8
nchchow <nelsonchow91.nc@gmail.com>
2020-02-05 14:46:44 -0800

    complete leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(divisibleByFourHundred(year)) {
+        return true;
+    }
+    if(divisibleByOneHundred(year)) {
+        return false;
+    }
+    if(divisibleByFour(year)) {
+        return true;
+    }
+    return false;
+}
 
+const divisibleByFour = function(number) {
+    return (number % 4 === 0) ? true : false;
+}
+
+const divisibleByOneHundred = function(number) {
+    return (number % 100 === 0) ? true : false;
+}
+
+const divisibleByFourHundred = function(number) {
+    return (number % 400 === 0) ? true : false;
 }
 
 module.exports = leapYears
