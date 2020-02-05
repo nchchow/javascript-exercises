f1d461a0b3ed09b437f5a653531f0cf7755ff159
nchchow <nelsonchow91.nc@gmail.com>
2020-02-05 13:55:01 -0800

    complete repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (string != '' && times >= 0) {
+        newString = '';
+        for (i = 0; i < times; ++i) {
+            newString += string;
+        }
+        return newString;
+    } else {
+        return 'ERROR';
+    }
 }
 
 module.exports = repeatString
