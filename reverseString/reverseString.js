585737b3300d0d1b57a1b69057fc4995438b82fc
nchchow <nelsonchow91.nc@gmail.com>
2020-02-05 13:54:10 -0800

    completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let result = '';
+    for(i = (stringToReverse.length - 1); i >= 0; --i) {
+        result += stringToReverse[i];
+    }
+    return result;
 }
 
 module.exports = reverseString
