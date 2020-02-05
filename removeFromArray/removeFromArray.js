c03f1549cb3243803afa98c1592e7994ac569673
nchchow <nelsonchow91.nc@gmail.com>
2020-02-05 14:25:47 -0800

    complete removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...values) {
+    for (var i = 0; i < array.length; ++i) {
+        for (var j = 0; j < values.length; ++j) {
+            if (array[i] === values[j]) {
+                array.splice(i, 1);
+                --i;
+            }
+        }
+    }
+    return array;
 }
 
 module.exports = removeFromArray
