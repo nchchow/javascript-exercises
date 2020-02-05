6dfe2a5c96d266728fdfc32a76282ea5073bc8db
nchchow <nelsonchow91.nc@gmail.com>
2020-02-05 14:34:01 -0800

    complete sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(typeof start != 'number' || typeof end != 'number') {
+        return 'ERROR';
+    }
+    if(start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    if(end < start) {
+        var tmp = start;
+        start = end;
+        end = tmp;
+    }
+    for(i = start; i <= end; ++i) {
+        sum += i;
+    }
+    return sum;
 }
 
 module.exports = sumAll
