1fbda2dd18882f0e778dcf00888cea27eded40d1
nchchow <nelsonchow91.nc@gmail.com>
2020-02-05 14:55:59 -0800

    complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  var c = (f - 32) * 5 / 9;
+  return Math.round(c * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(c) {
+  f = c * 9 / 5 + 32; 
+  return Math.round(f * 10) / 10;
 }
 
 module.exports = {
