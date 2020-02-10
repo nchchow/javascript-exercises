399df518126985393464a756ec2d2ba47b465d07
nchchow <nelsonchow91.nc@gmail.com>
2020-02-09 17:06:57 -0800

    Complete palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function() {
+const punctuations = "!@#$%^&*().,;'\" ";   // includes spaces
 
+const palindromes = function(string) {
+    let removed = removePunctuations(string);
+    return removed === reverseString(removed);
+}
+
+function removePunctuations(string) {
+    return string.toLowerCase().split('')                       // split into array
+                .filter(char => (!punctuations.includes(char))) // filter out punctuations
+                .join('');                                      // join into string
+}
+
+function reverseString(string) {
+    return string.split('').reverse().join('');
 }
 
 module.exports = palindromes
